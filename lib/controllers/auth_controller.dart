0ce2e978a303581385a21651514706f03e5f8f6b
Liul-Abera <liulaber43@gmail.com>
2025-04-09 16:11:36 +0300

    upload image

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class AuthController {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
   pickProfileImage(ImageSource source) async {
@@ -17,23 +20,37 @@ class AuthController {
     }
   }
 
+  // function to  to upload image to firebase storage
+  _uploadImageToStorage(Uint8List? image) async {
+    Reference ref = _storage
+        .ref()
+        .child('profileImages')
+        .child(_auth.currentUser!.uid);
+    UploadTask uploadTask = ref.putData(image!);
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
   Future<String> createNewUser({
     required String email,
     required String fullName,
     required String password,
+    required Uint8List? image,
   }) async {
     String res = 'some error Occurred';
     try {
       // Create user with email and password
       UserCredential userCredential = await _auth
           .createUserWithEmailAndPassword(email: email, password: password);
-
+      String downloadUrl = await _uploadImageToStorage(image);
       // Store user data in Firestore
       await _firebaseFirestore
           .collection('buyers')
           .doc(userCredential.user!.uid)
           .set({
             'fullName': fullName,
+            'profileImages': downloadUrl,
             'email': email,
             'buyerId': userCredential.user!.uid,
           });
--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -42,150 +42,157 @@ class _RegisterScreenState extends State<RegisterScreen> {
         ),
         child: Form(
           key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Register',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 4,
-                  fontSize: 22,
-                ),
-              ),
-              SizedBox(height: 20),
-              Stack(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  _image == null
-                      ? CircleAvatar(
-                        radius: 56,
-                        child: Icon(Icons.person, size: 70),
-                      )
-                      : CircleAvatar(
-                        radius: 56,
-                        backgroundImage: MemoryImage(_image!),
-                      ),
-                  Positioned(
-                    right: 0,
-                    top: 20,
-                    child: IconButton(
-                      icon: Icon(CupertinoIcons.photo),
-                      onPressed: () {
-                        selectGalleryImage();
-                      },
+                  Text(
+                    'Register',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 4,
+                      fontSize: 22,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) {
-                  email = value;
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please Email Address must Not Be Empty';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  label: Text('Email'),
-                  hintText: 'Enter your email',
-                  prefixIcon: Icon(Icons.email),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) {
-                  fullName = value;
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Full name must not be empty';
-                  }
-                  if (value.length < 3) {
-                    return 'Full name must be at least 3 characters';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  label: Text('Full name'),
-                  hintText: 'Full Name',
-                  prefixIcon: Icon(Icons.person),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) => password = value,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Password must not be empty';
-                  }
-                  if (value.length < 6) {
-                    return 'Password must be at least 6 characters';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  label: Text('Password'),
-                  hintText: 'Password ',
-                  prefixIcon: Icon(Icons.lock),
-                ),
-              ),
-              SizedBox(height: 20),
-              InkWell(
-                onTap: () {
-                  print('Register button clicked');
-                  if (_formKey.currentState!.validate()) {
-                    _authController.createNewUser(
-                      email: email,
-                      fullName: fullName,
-                      password: password,
-                    );
-                    print('successfully registered');
-                  } else {
-                    print('Not Validate');
-                  }
-                },
-                child: Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width - 40,
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
-                    border: Border.all(color: Colors.black, width: 1),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.2),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+                  SizedBox(height: 20),
+                  Stack(
+                    children: [
+                      _image == null
+                          ? CircleAvatar(
+                            radius: 56,
+                            child: Icon(Icons.person, size: 70),
+                          )
+                          : CircleAvatar(
+                            radius: 56,
+                            backgroundImage: MemoryImage(_image!),
+                          ),
+                      Positioned(
+                        right: 0,
+                        top: 20,
+                        child: IconButton(
+                          icon: Icon(CupertinoIcons.photo),
+                          onPressed: () {
+                            selectGalleryImage();
+                          },
+                        ),
                       ),
                     ],
                   ),
-                  child: Center(
-                    child: Text(
-                      "Register",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        letterSpacing: 2,
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (value) {
+                      email = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please Email Address must Not Be Empty';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      label: Text('Email'),
+                      hintText: 'Enter your email',
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (value) {
+                      fullName = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Full name must not be empty';
+                      }
+                      if (value.length < 3) {
+                        return 'Full name must be at least 3 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      label: Text('Full name'),
+                      hintText: 'Full Name',
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (value) => password = value,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Password must not be empty';
+                      }
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      label: Text('Password'),
+                      hintText: 'Password ',
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  InkWell(
+                    onTap: () {
+                      print('Register button clicked');
+                      if (_formKey.currentState!.validate()) {
+                        _authController.createNewUser(
+                          email: email,
+                          fullName: fullName,
+                          password: password,
+                          image: _image,
+                        );
+                        print('successfully registered');
+                      } else {
+                        print('Not Validate');
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width - 40,
+                      decoration: BoxDecoration(
+                        color: Colors.pink,
+                        border: Border.all(color: Colors.black, width: 1),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            letterSpacing: 2,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  TextButton(
+                    onPressed:
+                        () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        ),
+                    child: Text('Already Have An Account '),
+                  ),
+                ],
               ),
-              TextButton(
-                onPressed:
-                    () => Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()),
-                    ),
-                child: Text('Already Have An Account '),
-              ),
-            ],
+            ),
           ),
         ),
       ),
