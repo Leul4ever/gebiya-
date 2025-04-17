7d241a116f5ca857081ee772142c7d9c1d5240aa
Liul-Abera <liulaber43@gmail.com>
2025-04-17 21:48:14 +0300

    map

--- lib/views/auth/map_screen.dart
+++ lib/views/auth/map_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
@@ -18,6 +19,23 @@ class _MapScreenState extends State<MapScreen> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+  late Position currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserCurrentLocation();
+  }
+
+  getUserCurrentLocation() async {
+    await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.bestForNavigation,
+      forceAndroidLocationManager: true,
+    );
+    currentPosition = position;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +43,7 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           GoogleMap(
-            padding: EdgeInsets.only(bottom: 200),
+            padding: const EdgeInsets.only(bottom: 200),
             mapType: MapType.normal,
             initialCameraPosition: _kGooglePlex,
             onMapCreated: (GoogleMapController controller) {
@@ -44,7 +62,7 @@ class _MapScreenState extends State<MapScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
+                  SizedBox(
                     width: MediaQuery.of(context).size.width - 70,
                     child: ElevatedButton.icon(
                       onPressed: () {},
